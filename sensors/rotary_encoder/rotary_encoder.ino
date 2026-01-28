c3a1a90f2c0ed83a07fa878c1d4cca440ce753ee
Bas Baccarne <bastiaan.baccarne@ugent.be>
2026-01-28 14:56:14 +0100

    Improved rotary encoder pin code

--- sensors/rotary_encoder/rotary_encoder.ino
+++ sensors/rotary_encoder/rotary_encoder.ino
@@ -1,55 +1,55 @@
 // Rotary Encoder Example:
-// This code reads a rotary encoder connected to digital pins 2 and 3
+// This code reads a rotary encoder connected to analog pins 2 and 3
 // and prints the position to the Serial Monitor. It also detects button
 // presses on pin 4.
 
 // Wiring:
-// Connect the encoder's A output to pin D2
-// Connect the encoder's B output to pin D3
-// Connect the encoder's switch output to pin 4
+// Connect the encoder's A output to pin A2
+// Connect the encoder's B output to pin A3
+// Swap the pins when direction is detected wrong.
+// Connect the encoder's switch output to pin D4
 // Connect both grounds
 
-#define PIN_A 2   // Encoder output A
-#define PIN_B 3   // Encoder output B
-#define PIN_SW 4  // Encoder push-button
+// install RotaryEncoder by Matthias Hertel
+#include <RotaryEncoder.h>
 
-int position = 0;  // Variable to store encoder position
-int lastA = HIGH;  // Variable to store last state of A
+#define PIN_IN1 A2
+#define PIN_IN2 A3
+#define PIN_SW 4
+
+// Change to the appropirate code below
+
+// Option 1: Setup a RotaryEncoder with 4 steps per latch for the 2 signal input
+// pins:
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+
+// Option 2: Setup a RotaryEncoder with 2 steps per latch for the 2 signal input
+// pins: RotaryEncoder encoder(PIN_IN1, PIN_IN2,
+// RotaryEncoder::LatchMode::TWO03);
 
 void setup() {
-  Serial.begin(9600);
-
-  pinMode(PIN_A, INPUT_PULLUP);
-  pinMode(PIN_B, INPUT_PULLUP);
+  Serial.begin(115200);
   pinMode(PIN_SW, INPUT_PULLUP);
 }
 
 void loop() {
-  int a = digitalRead(PIN_A);
-  int b = digitalRead(PIN_B);
+  static int pos = 0;
 
-  // Detect rising edge on A
-  // Logic:
-  // If A goes from LOW to HIGH, check B to determine direction
-  // If B is LOW, it's clockwise; if HIGH, it's counter-clockwise
+  encoder.tick();
 
-  if (a == HIGH && lastA == LOW) {
-    if (b == LOW)
-      position++;  // clockwise
-    else
-      position--;  // counter-clockwise
-    Serial.print("Position: ");
-    Serial.println(position);
+  // Get position and detect changes
+  int newPos = encoder.getPosition();
+  if (pos != newPos) {
+    Serial.print("pos:");
+    Serial.print(newPos);
+    Serial.print(" dir:");
+    Serial.println((int)(encoder.getDirection()));
+    pos = newPos;
   }
 
-  // Update lastA state
-  lastA = a;
-
-  // Simple Button press detection
+  // Detect button press
   if (digitalRead(PIN_SW) == LOW) {
     Serial.println("Button pressed!");
-    delay(200);  // debounce
+    delay(200);  // simple debounce
   }
-
-  delay(5);
 }
